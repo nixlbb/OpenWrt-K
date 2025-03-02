ed37ce881d765252572b4c7680ab628e11840529
nixlbb <65245624+nixlbb@users.noreply.github.com>
2025-03-02 22:55:50 +0800

    DIY

--- build_helper/prepare.py
+++ build_helper/prepare.py
@@ -94,7 +94,6 @@ def prepare(configs: dict[str, dict[str, Any]]) -> None:
     # clone拓展软件源码
     logger.info("开始克隆拓展软件源码...")
     to_clone: set[tuple[str, str]] = {("https://github.com/immortalwrt/packages", ""),
-                                       ("https://github.com/chenmozhijin/turboacc", "package"),
                                        *[(pkg["REPOSITORIE"], pkg["BRANCH"]) for config in configs.values() for pkg in config["extpackages"].values()],
                                        *[("https://github.com/sbwml/packages_lang_golang",
                                           config["openwrtext"]["golang_version"]) for config in configs.values()]}
@@ -154,38 +153,10 @@ def prepare(configs: dict[str, dict[str, Any]]) -> None:
         for name in cfg_names[1:]:
             shutil.copytree(os.path.join(openwrt_paths, cfg_names[0]), os.path.join(openwrt_paths, name), symlinks=True)
     openwrts = {name: OpenWrt(os.path.join(openwrt_paths, name), configs[name]["compile"]["openwrt_tag/branch"]) for name in cfg_names}
-
-    # 下载AdGuardHome规则与配置
-    logger.info("下载AdGuardHome规则与配置...")
-    global_files_path = os.path.join(paths.workdir, "files")
-    shutil.copytree(os.path.join(paths.openwrt_k, "files"), global_files_path, symlinks=True)
-    adg_filters_path = os.path.join(global_files_path, "usr", "bin", "AdGuardHome", "data", "filters")
-    os.makedirs(adg_filters_path, exist_ok=True)
-    filters = {"1628750870.txt": "https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt",
-               "1628750871.txt": "https://anti-ad.net/easylist.txt",
-               "1677875715.txt": "https://easylist-downloads.adblockplus.org/easylist.txt",
-               "1677875716.txt": "https://easylist-downloads.adblockplus.org/easylistchina.txt",
-               "1677875717.txt": "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt",
-               "1677875718.txt": "https://raw.githubusercontent.com/zsakvo/AdGuard-Custom-Rule/master/rule/zhihu-strict.txt",
-               "1677875720.txt": "https://gist.githubusercontent.com/Ewpratten/a25ae63a7200c02c850fede2f32453cf/raw/b9318009399b99e822515d388b8458557d828c37/hosts-yt-ads",
-               "1677875724.txt": "https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt",
-               "1677875725.txt": "https://www.i-dont-care-about-cookies.eu/abp/",
-               "1677875726.txt": "https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts",
-               "1677875727.txt": "https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts",
-               "1677875728.txt": "https://winhelp2002.mvps.org/hosts.txt",
-               "1677875733.txt": "https://raw.githubusercontent.com/hl2guide/Filterlist-for-AdGuard/master/filter_whitelist.txt",
-               "1677875734.txt": "https://raw.githubusercontent.com/hg1978/AdGuard-Home-Whitelist/master/whitelist.txt",
-               "1677875735.txt": "https://raw.githubusercontent.com/mmotti/adguard-home-filters/master/whitelist.txt",
-               "1677875737.txt": "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/white.txt",
-               "1677875739.txt": "https://raw.githubusercontent.com/JamesDamp/AdGuard-Home---Personal-Whitelist/master/AdGuardHome-Whitelist.txt",
-               #"1677875740.txt": "https://raw.githubusercontent.com/scarletbane/AdGuard-Home-Whitelist/main/whitelist.txt"
     }
     dl_tasks: list[DLTask] = []
     for name, url in filters.items():
         dl_tasks.append(dl2(url, os.path.join(adg_filters_path, name)))
-
-    dl_tasks.append(dl2("https://raw.githubusercontent.com/chenmozhijin/AdGuardHome-Rules/main/AdGuardHome-dnslist(by%20cmzj).yaml",
-                     os.path.join(global_files_path, "etc", "AdGuardHome-dnslist(by cmzj).yaml")))
 
     wait_dl_tasks(dl_tasks)
 
@@ -235,64 +206,6 @@ def prepare_cfg(config: dict[str, Any],
     openwrt.apply_config(config["openwrt"])
     openwrt.make_defconfig()
     config["openwrt"] = openwrt.get_diff_config()
-
-    # 添加turboacc补丁
-    turboacc_dir = os.path.join(cloned_repos[("https://github.com/chenmozhijin/turboacc", "package")])
-    kernel_version = openwrt.get_kernel_version()
-    enable_sfe = (openwrt.get_package_config("kmod-shortcut-fe") in ("y", "m") or
-               openwrt.get_package_config("kmod-shortcut-fe-drv") in ("y", "m") or
-               openwrt.get_package_config("kmod-shortcut-fe-cm") in ("y", "m") or
-               openwrt.get_package_config("kmod-fast-classifier") in ("y", "m"))
-    enable_fullcone = openwrt.get_package_config("kmod-nft-fullcone") in ("y", "m")
-    if enable_fullcone or enable_sfe:
-        logger.info("%s添加952补丁", cfg_name)
-        patch925 = f"952{"-add" if kernel_version != "5.10" else ""}-net-conntrack-events-support-multiple-registrant.patch"
-        shutil.copy2(os.path.join(turboacc_dir, f"hack-{kernel_version}", patch925),
-                     os.path.join(openwrt.path, "target", "linux", "generic", f"hack-{kernel_version}", patch925))
-        logger.info("%s附加内核配置CONFIG_NF_CONNTRACK_CHAIN_EVENTS", cfg_name)
-        with open(os.path.join(openwrt.path, "target", "linux", "generic", f"config-{kernel_version}"), "a") as f:
-            f.write("\n# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set")
-    if enable_sfe:
-        logger.info("%s添加953补丁", cfg_name)
-        patch953 = "953-net-patch-linux-kernel-to-support-shortcut-fe.patch"
-        shutil.copy2(os.path.join(turboacc_dir, f"hack-{kernel_version}", patch953),
-                     os.path.join(openwrt.path, "target", "linux", "generic", f"hack-{kernel_version}", patch953))
-        logger.info("%s添加613补丁", cfg_name)
-        patch613 = "613-netfilter_optional_tcp_window_check.patch"
-        shutil.copy2(os.path.join(turboacc_dir, f"pending-{kernel_version}", patch613),
-                     os.path.join(openwrt.path, "target", "linux", "generic", f"pending-{kernel_version}", patch613))
-        logger.info("%s附加内核配置CONFIG_SHORTCUT_FE", cfg_name)
-        with open(os.path.join(openwrt.path, "target", "linux", "generic", f"config-{kernel_version}"), "a") as f:
-            f.write("\nCONFIG_SHORTCUT_FE=y")
-    if enable_fullcone:
-        logger.info("%s添加libnftnl、firewall4、nftables补丁", cfg_name)
-        def get_version(file_path: str, pattern: str) -> str | None:
-            with open(file_path, encoding='utf-8') as f:
-                content = f.read()
-                match = re.search(pattern, content)
-            return match.group(1) if match else None
-
-        firewall4_ver = get_version(os.path.join(openwrt.path, "package", "network", "config", "firewall4", "Makefile"), r'PKG_SOURCE_VERSION:=(.*)')
-        nftables_ver = get_version(os.path.join(openwrt.path, "package", "network", "utils", "nftables", "Makefile"), r'PKG_VERSION:=(.*)')
-        libnftnl_ver = get_version(os.path.join(openwrt.path, "package", "libs", "libnftnl", "Makefile"), r'PKG_VERSION:=(.*)')
-        latest_versions = parse_config(os.path.join(turboacc_dir, "version"), ("FIREWALL4_VERSION", "NFTABLES_VERSION", "LIBNFTNL_VERSION"))
-        if not os.path.isdir(os.path.join(turboacc_dir, f"libnftnl-{libnftnl_ver}")):
-            logger.warning("%s未找到当前libnftnl版本%s，使用最新版本", cfg_name, libnftnl_ver)
-            libnftnl_ver = latest_versions["LIBNFTNL_VERSION"]
-        if not os.path.isdir(os.path.join(turboacc_dir, f"firewall4-{firewall4_ver}")):
-            logger.warning("%s未找到当前firewall4版本%s，使用最新版本", cfg_name, firewall4_ver)
-            firewall4_ver = latest_versions["FIREWALL4_VERSION"]
-        if not os.path.isdir(os.path.join(turboacc_dir, f"nftables-{nftables_ver}")):
-            logger.warning("%s未找到当前nftables版本%s，使用最新版本", cfg_name, nftables_ver)
-            nftables_ver = latest_versions["NFTABLES_VERSION"]
-        shutil.rmtree(os.path.join(openwrt.path, "package", "libs", "libnftnl"))
-        shutil.copytree(os.path.join(turboacc_dir, f"libnftnl-{libnftnl_ver}"), os.path.join(openwrt.path, "package", "libs", "libnftnl"))
-        shutil.rmtree(os.path.join(openwrt.path, "package", "network", "config", "firewall4"))
-        shutil.copytree(os.path.join(turboacc_dir, f"firewall4-{firewall4_ver}"),
-                        os.path.join(openwrt.path, "package", "network", "config", "firewall4"))
-        shutil.rmtree(os.path.join(openwrt.path, "package", "network", "utils", "nftables"))
-        shutil.copytree(os.path.join(turboacc_dir, f"nftables-{nftables_ver}"),
-                        os.path.join(openwrt.path, "package", "network", "utils", "nftables"))
 
     logger.info("%s准备自定义文件...", cfg_name)
     files_path = os.path.join(openwrt.path, "files")
@@ -384,9 +297,7 @@ def prepare_cfg(config: dict[str, Any],
                 os.chmod(os.path.join(clash_core_path, "clash"), 0o755)  # noqa: S103
 
     tmpdir.cleanup()
-
-    # 获取bt_trackers
-    bt_tracker = request_get("https://github.com/XIU2/TrackersListCollection/raw/master/all_aria2.txt")
+    
     # 替换信息
     with open(os.path.join(files_path, "etc", "uci-defaults", "zzz-chenmozhijin"), encoding="utf-8") as f:
         content = f.read()
